90c409fe5901454910b7d2c32f7b021fefba7ca5
alokrai0607 <saelalok001@gmail.com>
2023-08-04 20:08:15 +0530

    sum

---
+++ SumAndAvg.py
@@ -0,0 +1,11 @@
+# Given list (array) of numbers
+numbers = [10 ,20 ,30 ,40]
+sum = 0
+
+# Using a for loop to calculate the sum
+for i in numbers:
+    sum += i
+
+print("Sum of numbers:", sum)
+count = len(numbers)
+print("Avg is :" ,(sum/count))

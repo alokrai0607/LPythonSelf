3eda845b54b291282974b7c214d58ad6b404c296
alokrai0607 <saelalok001@gmail.com>
2023-08-05 15:52:58 +0530

    AppendString

---
+++ ConditionalList.py
@@ -0,0 +1,9 @@
+List = [12, 75, 150, 180, 145, 525, 50]
+
+for number in List:
+    if number % 5 == 0:
+        if number > 500:
+            break
+        elif number > 150:
+            continue
+        print(number)

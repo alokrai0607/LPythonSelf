ec682840074fbec06347dd984460c6dd81dfa483
alokrai0607 <saelalok001@gmail.com>
2023-08-06 09:01:06 +0530

    Selection sort

---
+++ SelectionSort.py
@@ -0,0 +1,18 @@
+def selection_sort(arr):
+    num = len(arr)
+
+    for i in range(num):
+        min_index = i
+
+        for j in range(i + 1, num):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+input_list = [64, 25, 12, 22, 11]
+
+sorted_list = selection_sort(input_list)
+print(sorted_list)

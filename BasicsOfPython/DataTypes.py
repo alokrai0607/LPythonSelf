b0afd406d8bfc0086f94ef8448a24293600f04b4
alokrai0607 <saelalok001@gmail.com>
2023-10-11 22:16:26 +0530

    Data Types

---
+++ BasicsOfPython/DataTypes.py
@@ -0,0 +1,12 @@
+
+name = "Alok"
+age = 25
+marks = 95
+height = 5.9
+male = True
+
+print("My Name is"+name)
+print("My age is",age)
+print("i got ",marks,"% marks in my Exams")
+print("My height is ",height, "inch")
+print("I am a male and this is ",male)

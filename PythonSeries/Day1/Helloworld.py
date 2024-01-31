b1dac0660b1a198c6707e662dbce9e8070914674
alokrai0607 <saelalok001@gmail.com>
2024-01-31 10:13:41 +0530

    Variable type

--- PythonSeries/Day1/Helloworld.py
+++ PythonSeries/Day1/Helloworld.py
@@ -1,1 +1,8 @@
+#print my first program
 print("Hello World")
+
+#Print in same line
+print("Hello world." , "Alok Here")
+
+#line break with \n
+print("Hello World ."+"\n"+"Alok Here.")
---
+++ PythonSeries/Day1/printVariables.py
@@ -0,0 +1,8 @@
+name = "Alok Kumar Rai"   #String
+age = 29  #Integer
+height = 5.7  #float
+
+
+print("My name is :",name)
+print("My age is :",age)
+print("My height is :",height)

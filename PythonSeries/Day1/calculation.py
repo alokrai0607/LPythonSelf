1ad67fa3ddc471c54d137c3a6585788336ec954b
alokrai0607 <saelalok001@gmail.com>
2024-01-31 11:09:22 +0530

    Hello py

---
+++ PythonSeries/Day1/calculation.py
@@ -0,0 +1,28 @@
+
+#method 1 (sum)
+a = 2 
+b = 5 
+sum = a+b
+print (sum)
+
+#method 2 (sub)
+a = 500
+b = 100
+diff = a-b
+print(diff)
+
+#method 3 (Multipluy)
+a=10
+b=9
+mul = a*b
+print(mul)
+
+#method 4 (div)
+a = 10
+b = 2
+div = a/b
+print(round(div))
+
+
+
+ 

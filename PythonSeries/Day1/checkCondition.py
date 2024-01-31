029e2e79c29c87f3bab4e0e883f724bbf743d3be
alokrai0607 <saelalok001@gmail.com>
2024-01-31 13:13:51 +0530

    Day1

---
+++ PythonSeries/Day1/checkCondition.py
@@ -0,0 +1,4 @@
+value1 = int(input("First Number :"))
+value2 = int(input("Second Number :"))
+
+print(value1>=value2)
---
+++ PythonSeries/Day1/printavg.py
@@ -0,0 +1,4 @@
+
+val1 = float(input ("pass your first Number :"))
+val2 = float(input ("pass your second Number :"))
+print((val1+val2)/2)

ce40ac612514f81b9391ab8fda042f12fb38607b
alokrai0607 <saelalok001@gmail.com>
2024-01-31 12:57:29 +0530

    Programs

---
+++ PythonSeries/Day1/inputAndSum.py
@@ -0,0 +1,6 @@
+
+
+a = int(input("pass your first Int Number Here :" ))
+b = int(input("pass your Second Int Number Here :" ))
+print ("result will be of Sum :",a+b)
+
---
+++ PythonSeries/Day1/printArea.py
@@ -0,0 +1,4 @@
+
+a = int(input("pass your side length of square :"))
+area = a*a
+print("Area of Aquare will be :",area)

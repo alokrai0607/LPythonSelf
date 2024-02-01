9868be554ef6628f135aa88d65a8eb144d07a7d0
alokrai0607 <saelalok001@gmail.com>
2024-02-01 15:05:14 +0530

    Len

--- PythonSeries/Day2/Sliceing.py
+++ PythonSeries/Day2/Sliceing.py
@@ -1,5 +1,7 @@
 str = "AlokKumarRai"
-print(str[4:9])
+print(str[4:9])   #Kumar
+print(str[:len(str)]) #AlokKumarRai
+print(str[4:len(str)]) #KumarRai
 
 #Fourth index can include but End index will not be include in slice case.
 

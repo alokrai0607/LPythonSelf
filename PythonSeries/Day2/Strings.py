b50f77e17fa0e809f5ef63eb4a7373c0cfefc468
alokrai0607 <saelalok001@gmail.com>
2024-02-01 14:31:47 +0530

    String Writing ways

---
+++ PythonSeries/Day2/Strings.py
@@ -0,0 +1,7 @@
+str1 = 'this is single quote string'
+str2 = "this is double qiote string"
+str3 = """This is tripple quote string """
+
+#   All string are valid because in python we can decleare string in these all three ways
+
+print(str1+"\n"+str2+"\n"+str3)

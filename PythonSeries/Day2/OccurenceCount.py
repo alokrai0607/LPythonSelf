2ca3001b37d6d3f7b8d160d4e283d5d227f1a68d
alokrai0607 <saelalok001@gmail.com>
2024-02-01 18:46:56 +0530

    Programs in py

---
+++ PythonSeries/Day2/OccurenceCount.py
@@ -0,0 +1,5 @@
+
+# it is hard code for check doller occurence in our program .
+variable = input("Pass your String which you want :")
+print("Your occurence of '$' in your string will be :",variable.count("$"))
+

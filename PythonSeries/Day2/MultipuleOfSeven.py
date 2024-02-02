e64ed931977da38c0d54fad2c5754ddf45bf0072
alokrai0607 <saelalok001@gmail.com>
2024-02-02 19:51:24 +0530

    Mul of 7

---
+++ PythonSeries/Day2/MultipuleOfSeven.py
@@ -0,0 +1,6 @@
+Number = int(input("Please pass your Number I will check it is multipule of seven or Not : "))
+
+if(Number%7==0):
+    print(Number , "is Multipule of 7")
+else:
+    print(Number , "is Not Nultipue of 7")

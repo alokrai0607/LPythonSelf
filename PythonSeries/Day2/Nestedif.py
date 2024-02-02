a716772a470385466bfdbe286211859e180cdb6e
alokrai0607 <saelalok001@gmail.com>
2024-02-02 19:24:04 +0530

    Nested If

---
+++ PythonSeries/Day2/Nestedif.py
@@ -0,0 +1,9 @@
+age = int(input("Please pass your age Here we will check you are valid or Not for Drive:"))
+
+if(age>=18):
+    if(age>=80):
+        print("Can't drive")
+    else:
+        print("can drive")
+else:
+    print("driver is miner")

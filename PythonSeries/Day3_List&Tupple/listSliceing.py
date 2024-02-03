e2dd43468ee0d9eee0d4c7bb8a24659baf416a2d
alokrai0607 <saelalok001@gmail.com>
2024-02-03 21:39:58 +0530

    Listsliceing

--- PythonSeries/Day3_List&Tupple/listSliceing.py
+++ PythonSeries/Day3_List&Tupple/listSliceing.py
@@ -4,3 +4,6 @@ print(list[0:3])  #[21, 25, 90]
 print(list[:3]) #[21, 25, 90]
 print(list[0:len(list)])#[21, 25, 90, 85, 68, 78]
 print(list[1:len(list)]) #[25, 90, 85, 68, 78]
+
+#Negative index sliceing
+print(list[-5:-2])

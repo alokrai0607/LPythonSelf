5f7762d1b5e3361719e120ad38cfe085cb5d2dfe
alokrai0607 <saelalok001@gmail.com>
2024-02-03 14:41:15 +0530

    List

---
+++ PythonSeries/Day3_List&Tupple/list.py
@@ -0,0 +1,7 @@
+marks = [90.5 , 95.3 , 85.5 , 49.7 , 66.0]
+print(marks)    #[90.5, 95.3, 85.5, 49.7, 66.0]
+print(type(marks))  #<class 'list'>
+
+#if we want to access element on index bases.
+print(marks[0])   #90.5
+print(marks[4])   #66.0

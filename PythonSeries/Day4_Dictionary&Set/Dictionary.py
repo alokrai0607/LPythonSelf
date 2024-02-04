a2ce499415537299566292e18f1795251fac7001
alokrai0607 <saelalok001@gmail.com>
2024-02-04 17:12:35 +0530

    Hello dic

---
+++ PythonSeries/Day4_Dictionary&Set/Dictionary.py
@@ -0,0 +1,13 @@
+StudentInfo = {
+    "StName" : "Alok",
+    "Subject":["Java","Python","JavaScript","CSS","HTML"],
+    "coNo":('9044414580','7880667919','7007304517'),
+    "age" : 29,
+    "marks":95.5,
+    "Male":True
+}
+
+print(StudentInfo)  # {'StName': 'Alok',
+                    #'Subject': ['Java', 'Python', 'JavaScript', 'CSS', 'HTML'], 
+                    #'coNo': ('9044414580', '7880667919', '7007304517'), 
+                    #'age': 29, 'marks': 95.5, 'Male': True}

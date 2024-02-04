b388e9ed5707bd7be51c6fe035f389b1a106a1d3
alokrai0607 <saelalok001@gmail.com>
2024-02-04 15:26:34 +0530

    Plendrome check

--- PythonSeries/Day3_List&Tupple/checkPelindrome.py
+++ PythonSeries/Day3_List&Tupple/checkPelindrome.py
@@ -1,15 +1,15 @@
-# list = [1,2,1]
+list = [1,2,1]
 
-# list_copy = list.copy()
-# list_copy.reverse()
+list_copy = list.copy()
+list_copy.reverse()
 
-# if(list == list_copy):
-#     print("it is pellindrome")
-# else:
-#     print("Not Pellindrome")
+if(list == list_copy):
+    print("it is pellindrome")
+else:
+    print("Not Pellindrome")
 
 
-    #Check Pellindrome on pellindrome check .
+#Check Pellindrome on pellindrome check .
 
 str = ["a","l","o","k","k","o","l","a","m"]
 strcopy = str.copy()

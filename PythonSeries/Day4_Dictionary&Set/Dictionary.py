af9a2d07b978e863a069ee90281c6ecfb8c841df
alokrai0607 <saelalok001@gmail.com>
2024-02-08 20:15:55 +0530

    Dictionary

--- PythonSeries/Day4_Dictionary&Set/Dictionary.py
+++ PythonSeries/Day4_Dictionary&Set/Dictionary.py
@@ -12,4 +12,19 @@ print(StudentInfo)  # {'StName': 'Alok',
                     #'coNo': ('9044414580', '7880667919', '7007304517'), 
                     #'age': 29, 'marks': 95.5, 'Male': True}
 
-                    
+print(type(StudentInfo))  #<class 'dict'>
+
+#Print by indexes
+
+print(StudentInfo["StName"])  #Alok
+print(StudentInfo["Subject"])  # ['Java', 'Python', 'JavaScript', 'CSS', 'HTML']
+print(StudentInfo["coNo"]) #('9044414580', '7880667919', '7007304517')
+print(StudentInfo["age"]) #29
+print(StudentInfo["marks"]) #95.5
+print(StudentInfo["Male"])  #True
+
+
+
+
+
+

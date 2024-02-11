a02998bdaf17e81078db848422a7be26e592391b
alokrai0607 <saelalok001@gmail.com>
2024-02-11 12:10:21 +0530

    Set

--- PythonSeries/Day4_Dictionary&Set/NestedDictonary.py
+++ PythonSeries/Day4_Dictionary&Set/NestedDictonary.py
@@ -12,6 +12,8 @@ print(Student) #{'name': 'Alok', 'score': {'chem': 95, 'math': 90, 'phy': 92}}
 
 
 print(Student["score"]) #{'chem': 95, 'math': 90, 'phy': 92}
+print(Student.get("score1"))  #None
+print(Student.get("score"))  #{'chem': 95, 'math': 90, 'phy': 92}
 
 
 #if we want to return all keys present in dictionary
@@ -26,6 +28,14 @@ print(list(Student.values()))  #['Alok', {'chem': 95, 'math': 90, 'phy': 92}]
 
 #return all key value pairs as tupple
 print(Student.items()) #dict_items([('name', 'Alok'), ('score', {'chem': 95, 'math': 90, 'phy': 92})])
+
+#Update Student
+Student.update({"city" : "Delhi"})
+print(Student)   #{'name': 'Alok', 'score': {'chem': 95, 'math': 90, 'phy': 92}, 'city': 'Delhi'}
+
+Student.update({"city" : "Mumbai"})
+print(Student)   #{'name': 'Alok', 'score': {'chem': 95, 'math': 90, 'phy': 92}, 'city': 'Mumbai'}
+
 
 
 

241fde51c9fe9e1e58869b3f8d1b024b50a9a79c
alokrai0607 <saelalok001@gmail.com>
2024-02-11 17:40:20 +0530

    loop

--- PythonSeries/Day5_LoopsinPython/Loops.py
+++ PythonSeries/Day5_LoopsinPython/Loops.py
@@ -6,7 +6,15 @@
 
 #While loop example
 
-count =1
+count =1  #iterator
 while count <= 10 :
     print("Hello count :",count)
     count+=1
+
+#print numbers
+    i = 1
+    while i <= 5 :
+        print(i)
+        i += 1 
+
+        

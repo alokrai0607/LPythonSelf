85b66ccd68eac2fce90498d9325a3b162a1dac5d
alokrai0607 <saelalok001@gmail.com>
2024-02-12 13:42:12 +0530

    break

---
+++ PythonSeries/Day5_LoopsinPython/BreakandContinue.py
@@ -0,0 +1,12 @@
+#Break: used to terminate the loop when encountered .
+
+#Continue : terminate exicutaion in the cuurent iteration and continious execution of the loop with nest iteration .
+
+
+
+i = 1
+while i<=5:
+    print(i)
+    if(i==3):
+        break
+    i += 1

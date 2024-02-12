2278810d9867bd497348851d4902fa23594f670f
alokrai0607 <saelalok001@gmail.com>
2024-02-12 13:53:13 +0530

    continue

--- PythonSeries/Day5_LoopsinPython/continue.py
+++ PythonSeries/Day5_LoopsinPython/continue.py
@@ -1,7 +1,26 @@
-i = 0
-while i<=5:
-    if(i==3):
-        i += 1
+# i = 0
+# while i<=5:
+#     if(i==3):
+#         i += 1
+#         continue
+#     print(i)
+#     i +=1
+
+
+#print Odd
+i=1
+while(i<=50):
+    if(i%2==0):
+        i+=1
         continue
     print(i)
-    i +=1
+    i+=1
+
+#print Even
+i=1
+while(i<=50):
+    if(i%2!=0):
+        i+=1
+        continue
+    print(i)
+    i+=1

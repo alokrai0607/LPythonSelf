8d2995d6e0cb29248d2935137ed2834005c1d853
alokrai0607 <saelalok001@gmail.com>
2024-02-13 16:50:43 +0530

    Hello

--- PythonSeries/Day6_Functions&Recursion/function.py
+++ PythonSeries/Day6_Functions&Recursion/function.py
@@ -1,5 +1,20 @@
-def addTwo(a,b):
+
+#What is def?
+#def : function defination it is .
+
+def addTwo(a,b):  #Parameaters
     return a+b
 
-a=addTwo(9,7)
+a=addTwo(9,7)  #function call , arguments
 print(a)
+
+
+
+def printhello():
+    print("Hello")
+    
+printhello()
+printhello()
+printhello()
+printhello()
+printhello()

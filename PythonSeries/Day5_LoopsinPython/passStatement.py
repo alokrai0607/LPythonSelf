53920f25df62bdfb28e8ea04008488763c0e0af4
alokrai0607 <saelalok001@gmail.com>
2024-02-13 14:12:40 +0530

    Hello pass

---
+++ PythonSeries/Day5_LoopsinPython/passStatement.py
@@ -0,0 +1,4 @@
+# Pass is null statement that dose nothing it is used as a placeholder for future code .
+for i in range(10):
+    pass  #work like continue but diffrent from continue .
+print("Some other works will start from here")

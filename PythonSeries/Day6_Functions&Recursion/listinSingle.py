bf0324e6221bc21d24e4f2a0a54092aa354484ca
alokrai0607 <saelalok001@gmail.com>
2024-02-14 19:55:57 +0530

    Hello Print

--- PythonSeries/Day6_Functions&Recursion/listinSingle.py
+++ PythonSeries/Day6_Functions&Recursion/listinSingle.py
@@ -2,8 +2,8 @@ cities =["Mumbai","Chennai","Ahamdabad","Delhi","Banglore","Hydrabad"]
 
 
 
-for i in cities:
-    print(" ".join(i))
+# for i in cities:
+#     print(" ".join(i))
 
 # M u m b a i
 # C h e n n a i
@@ -12,6 +12,13 @@ for i in cities:
 # B a n g l o r e
 # H y d r a b a d
     
-for i in cities:
-    print(i,end=" ")   #Mumbai Chennai Ahamdabad Delhi Banglore Hydrabad
+# for i in cities:
+#     print(i,end=" ")   #Mumbai Chennai Ahamdabad Delhi Banglore Hydrabad
 
+#printFunction
+    
+def print_list(list):
+    for i in list:
+        print(i,end=" ")
+
+print_list(cities) #Mumbai Chennai Ahamdabad Delhi Banglore Hydrabad 

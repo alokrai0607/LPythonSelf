f5a6b80760613a8ecd2cba97e1e6ac0408b39e3d
alokrai0607 <saelalok001@gmail.com>
2024-02-14 19:49:41 +0530

    printHorizantal

---
+++ PythonSeries/Day6_Functions&Recursion/listinSingle.py
@@ -0,0 +1,17 @@
+cities =["Mumbai","Chennai","Ahamdabad","Delhi","Banglore","Hydrabad"]
+
+
+
+for i in cities:
+    print(" ".join(i))
+
+# M u m b a i
+# C h e n n a i
+# A h a m d a b a d
+# D e l h i
+# B a n g l o r e
+# H y d r a b a d
+    
+for i in cities:
+    print(i,end=" ")   #Mumbai Chennai Ahamdabad Delhi Banglore Hydrabad
+

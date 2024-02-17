c0c7ad6980fc60bbb5377664812e0f51e8deefaa
alokrai0607 <saelalok001@gmail.com>
2024-02-17 14:15:30 +0530

    change

--- PythonSeries/Day7_FileIO/Override/override.py
+++ PythonSeries/Day7_FileIO/Override/override.py
@@ -1,3 +1,13 @@
+# Edit file (r+)
 f = open(r"C:\Users\91904\OneDrive\Desktop\LPythonSelf\PythonSeries\Day7_FileIO\Override\newt.txt", "r+")
 f.write("shiv")
+print(f.read())
 f.close()
+
+
+# Truncate (w+)
+f = open(r"C:\Users\91904\OneDrive\Desktop\LPythonSelf\PythonSeries\Day7_FileIO\Override\newt.txt", "w+")
+f.write("shiv")
+print(f.read())
+f.close()
+

28a1d068c2048e28bf58c500e6b141334149366a
alokrai0607 <saelalok001@gmail.com>
2024-02-20 17:23:48 +0530

    append+

--- PythonSeries/Day7_FileIO/Write_Append/writeAppend.py
+++ PythonSeries/Day7_FileIO/Write_Append/writeAppend.py
@@ -1,3 +1,3 @@
-f = open("C:/Users/91904/OneDrive/Desktop/LPythonSelf/PythonSeries/Day7_FileIO/Write_Append/writeAppend.txt", "a")
-f.write("\nHey this is my third line")
+f = open("C:/Users/91904/OneDrive/Desktop/LPythonSelf/PythonSeries/Day7_FileIO/Write_Append/writeAppend.txt", "a+")
+f.write("\nHey this is my fourth line")
 f.close()

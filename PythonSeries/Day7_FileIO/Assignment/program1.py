43c6936e5a20075d1e08db6efd1255185f9cc7be
alokrai0607 <saelalok001@gmail.com>
2024-02-24 21:39:09 +0530

    file

--- PythonSeries/Day7_FileIO/Assignment/program1.py
+++ PythonSeries/Day7_FileIO/Assignment/program1.py
@@ -1,5 +1,7 @@
 with open(r"C:\Users\91904\OneDrive\Desktop\LPythonSelf\PythonSeries\Day7_FileIO\Assignment\prectice.txt",'w') as f:
    
+
     f.write("Hi Everyone \n I am learning File I/o \n")
+   
    
     f.write("useing Python \n I like Program in Python .")

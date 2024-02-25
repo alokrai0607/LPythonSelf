e6600932d568dab19060a54cafa8465973ab8954
alokrai0607 <saelalok001@gmail.com>
2024-02-25 19:27:22 +0530

    Hello

--- DSA/QuickSort.py
+++ DSA/QuickSort.py
@@ -13,6 +13,7 @@ def quick_sort(arr):
     if len(arr) <= 1:
         return arr
 
+
     pivot = arr[len(arr) // 2]
     left = [x for x in arr if x < pivot]
     middle = [x for x in arr if x == pivot]

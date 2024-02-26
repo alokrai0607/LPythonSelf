e32593072b24469a8e4afa042b0220a236405338
alokrai0607 <saelalok001@gmail.com>
2024-02-26 18:48:26 +0530

    inserction

--- SortingAlgorithums/insertion_sort.py
+++ SortingAlgorithums/insertion_sort.py
@@ -4,13 +4,17 @@
 # The sorted part starts with the first element and grows by one element on each iteration.
 
 
+
 def insertion_sort(arr):
+
     n = len(arr)
     for i in range(1, n):
         key = arr[i]
         j = i - 1
+       
         while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
+
     return arr

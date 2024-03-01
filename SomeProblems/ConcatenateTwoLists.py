80e9831d8c9f20eb11632d9df34e1739dec2ad5e
alokrai0607 <saelalok001@gmail.com>
2024-03-01 22:47:33 +0530

    Hello dear

--- SomeProblems/ConcatenateTwoLists.py
+++ SomeProblems/ConcatenateTwoLists.py
@@ -1,10 +1,13 @@
 list1 = ["Hello ", "take "]
+
 list2 = ["Dear", "Sir"]
 
 result = []
 
 for item1 in list1:
+  
     for item2 in list2:
+  
         result.append(item1 + item2)
 
 print(result)

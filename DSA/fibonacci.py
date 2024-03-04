2a3233840e55f775618ca872d5775091824b3752
alokrai0607 <saelalok001@gmail.com>
2024-03-04 18:43:42 +0530

    asdfg

--- DSA/fibonacci.py
+++ DSA/fibonacci.py
@@ -2,13 +2,13 @@ def fibonacci(n):
      # Initialize the sequence with the first two Fibonacci numbers
   
   
-    sequence = [0, 1] 
+    seq = [0, 1] 
 
-    while len(sequence) < n:
-        next_number = sequence[-1] + sequence[-2]
-        sequence.append(next_number)
+    while len(seq) < n:
+        next_number = seq[-1] + seq[-2]
+        seq.append(next_number)
 
-    return sequence
+    return seq
 
 # Test the function with the given input
 inpnumber = 5

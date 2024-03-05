9393385377235323740987038a4fae38590ec876
alokrai0607 <saelalok001@gmail.com>
2024-03-05 20:19:53 +0530

    change

--- DSA/LongestCommonPrefix.py
+++ DSA/LongestCommonPrefix.py
@@ -9,16 +9,19 @@ def longest_common_prefix(strs):
 
     # Find the common prefix between the first and last strings
     common_prefix = []
+
     for i in range(min(len(first_str), len(last_str))):
         if first_str[i] == last_str[i]:
             common_prefix.append(first_str[i])
         else:
             break
 
+
     return "".join(common_prefix)
 
 # Input
 str_list = ["flower", "flow", "flight"]
+
 
 # Output
 output = longest_common_prefix(str_list)

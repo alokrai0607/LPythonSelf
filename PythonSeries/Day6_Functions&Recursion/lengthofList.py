ddd87f345b8a5a6a53d63faa35e05160c1215a49
alokrai0607 <saelalok001@gmail.com>
2024-03-06 18:46:10 +0530

    lengthOfList

--- PythonSeries/Day6_Functions&Recursion/lengthofList.py
+++ PythonSeries/Day6_Functions&Recursion/lengthofList.py
@@ -1,10 +1,13 @@
 
 
 cities =["Mumbai","Chennai","Ahamdabad","Delhi","Banglore","Hydrabad"]
+
 heros = ["Shaktiman","Aryaman","JuniorG","Thor","IronMan"]
 
 def printLength(list):
+
     print(len(list))
 
 printLength(cities)
+
 printLength(heros)

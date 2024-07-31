980b634a9c3aff7971d9c6d8472c4da437ed33c4
alokrai0607 <saelalok001@gmail.com>
2024-07-31 16:36:06 +0530

    Python test

--- PythonSeries/Day1_IntroductionOfPython/calculation.py
+++ PythonSeries/Day1_IntroductionOfPython/calculation.py
@@ -3,25 +3,25 @@
 a = 2 
 b = 5 
 sum = a+b
-print (sum)
+print ("Sum is" , sum)
 
 #method 2 (sub)
 a = 500
 b = 100
 diff = a-b
-print(diff)
+print("Sub is" , diff)
 
 #method 3 (Multipluy)
 a=10
 b=9
 mul = a*b
-print(mul)
+print("multi  is" , mul)
 
 #method 4 (div)
 a = 10
 b = 2
 div = a/b
-print(round(div))
+print("Div is" , round(div))
 
 
 
